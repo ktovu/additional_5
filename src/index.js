477b437fd34e8500a21a189ef9514916e3920b9e
ktovu <andrew.konash@yandex.ru>
2018-03-01 17:50:29 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var mass = [];
+  for (var a = 0; a < str.length; a++) {
+    for (var b = 0; b < bracketsConfig.length; b++) {
+      if (str[a] == bracketsConfig[b][0]) {
+        mass.push(str[a]);
+      }
+      if (str[a] == bracketsConfig[b][1]) {
+        mass.pop(str[a]);
+      }
+    }
+  }
+  if (mass == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
